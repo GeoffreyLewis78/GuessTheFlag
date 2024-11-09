bba4eaf3ac53df7be62569b02b4974d33e983a18
Geoff Lewis <64102317+GeoffreyLewis78@users.noreply.github.com>
2024-11-09 16:52:25 +0000

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Geoff Lewis on 09/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
